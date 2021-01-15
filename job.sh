32db3afc3001f5a2ad438f753756bc227d09726f
Christopher Ravn Boe Jensen <DG@Christophers-MacBook-Pro-5.local>
2021-01-15 18:21:50 +0100

    train

--- job.sh
+++ job.sh
@@ -9,7 +9,6 @@
 #BSUB -J My_Test_HPC
 ### specify the memory needed
 #BSUB -R "rusage[mem=32GB]"
-#BSUB -R "select[gpu32gb]"
 ### Number of hours needed
 #BSUB -W 23:59
 ### added outputs and errors to files
